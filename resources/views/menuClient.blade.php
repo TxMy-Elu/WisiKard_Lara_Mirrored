46b5f776707a47e90a2a46961cd02b0dbbcb7072
TxMy <to.doguet@gmail.com>
2025-01-13 09:40:22 +0100

    Update menuClient.blade.php

--- resources/views/menuClient.blade.php
+++ resources/views/menuClient.blade.php
@@ -6,12 +6,16 @@
             <div class="w-full lg:flex lg:flex-col lg:items-center lg:w-auto flex-grow">
                 <ul class="flex flex-col items-center lg:ml-0 flex-grow">
                     <li class="nav-item mb-4">
-                        <button class="mb-14 px-3 py-2 border rounded text-gray-700 border-gray-700 hidden" type="button" id="closeNavbarButton" onclick="toggleNavbar()">
-                            <svg class="w-6 h-6" fill="none" stroke="#ffff" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12"></path>
+                        <button class="mb-14 px-3 py-2 border rounded text-gray-700 border-gray-700 hidden"
+                                type="button" id="closeNavbarButton" onclick="toggleNavbar()">
+                            <svg class="w-6 h-6" fill="none" stroke="#ffff" viewBox="0 0 24 24"
+                                 xmlns="http://www.w3.org/2000/svg">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                      d="M6 18L18 6M6 6l12 12"></path>
                             </svg>
                         </button>
-                        <img class="origin-center rotate-45" src="{{ asset('icons/qr.svg') }}" alt="entreprise" width="40" height="40">
+                        <img class="origin-center rotate-45" src="{{ asset('icons/qr.svg') }}" alt="entreprise"
+                             width="40" height="40">
                     </li>
                     <div class="mt-14">
                         <li class="nav-item mb-4">
@@ -40,11 +44,20 @@
                             </a>
                         </li>
                     </div>
+                </ul>
+                <ul class="flex flex-col items-center lg:ml-0 mt-auto">
+                    <li class="nav-item mb-4">
+                        <a class="nav-link p-2 hover:text-red-500" href="{{ route('deconnexion') }}">
+                            <img src="{{ asset('icons/log-out.svg') }}" alt="deconnexion" width="30" height="30">
+                        </a>
+                    </li>
+                </ul>
             </div>
         </div>
     </nav>
 
-    <button class="block lg:hidden px-3 py-2 my-4 border rounded text-gray-700 border-gray-700" type="button" id="navbarToggleButton" onclick="toggleNavbar()">
+    <button class="block lg:hidden px-3 py-2 my-4 border rounded text-gray-700 border-gray-700" type="button"
+            id="navbarToggleButton" onclick="toggleNavbar()">
         <svg class="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg">
             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16m-7 6h7"></path>
         </svg>
