8c7d52a36d85b16d8803d643f91278990ceb19f5
TxMy <to.doguet@gmail.com>
2025-01-15 14:20:57 +0100

    update inscirption

--- app/Models/Compte.php
+++ app/Models/Compte.php
@@ -18,33 +18,37 @@ class Compte extends Model
     }
 
 
-    public static function existeEmail($email) {
+    public static function existeEmail($email)
+    {
         $nb = self::where("email", $email)->count();
 
-        if($nb > 0) {
+        if ($nb > 0) {
             return true;
-        }
-        else {
+        } else {
             return false;
         }
     }
 
-    public function estAdmin() {
+    public function estAdmin()
+    {
         return $this->role === 'admin';
     }
 
-    public function desactiverCompte() {
+    public function desactiverCompte()
+    {
         $this->estDesactiver = 1;
         $this->save();
     }
 
-    public function reactiverCompte() {
+    public function reactiverCompte()
+    {
         $this->estDesactiver = 0;
         $this->tentativesCo = 0;
         $this->save();
     }
 
-    public static function inscription($email, $motDePasseHache, $role) {
+    public static function inscription($email, $motDePasseHache, $role)
+    {
         $nouvelUtilisateur = new Compte();
         $nouvelUtilisateur->email = $email;
         $nouvelUtilisateur->password = $motDePasseHache;
@@ -52,17 +56,29 @@ class Compte extends Model
 
         $nouvelUtilisateur->save();
 
+        $entreprise = new Carte();
+        $entreprise->idCompte = $nouvelUtilisateur->idCompte;
+        $entreprise->nomEntreprise = "nomEntreprise";
+        $entreprise->titre = "titre";
+        $entreprise->tel = "tel";
+        $entreprise->ville = "ville";
+        $entreprise->idTemplate = 1;
+
+
+        $entreprise->save();
+
         return $nouvelUtilisateur->idCompte;
     }
-      public static function employe($nom, $prenom, $fonction)
-        {
-            // Créez un nouvel employé et enregistrez-le dans la base de données
-            $employe = new Employer();
-            $employe->nom = $nom;
-            $employe->prenom = $prenom;
-            $employe->fonction = $fonction;
-            $employe->save();
 
-            return $employe;
-        }
+    public static function employe($nom, $prenom, $fonction)
+    {
+        // Créez un nouvel employé et enregistrez-le dans la base de données
+        $employe = new Employer();
+        $employe->nom = $nom;
+        $employe->prenom = $prenom;
+        $employe->fonction = $fonction;
+        $employe->save();
+
+        return $employe;
+    }
 }
