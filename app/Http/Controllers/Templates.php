079c64c88d55b9d515d94c27321c8700ea5fe067
TxMy <to.doguet@gmail.com>
2025-01-16 10:07:40 +0100

    correctif

--- app/Http/Controllers/Templates.php
+++ app/Http/Controllers/Templates.php
@@ -16,13 +16,14 @@ class Templates extends Controller
 {
     public function afficherTemplates(Request $request)
     {
-
         $idCompte = $request->query('idCompte');
 
         $idTemplate = Carte::where('idCompte', $idCompte)->value('idTemplate');
 
         // Prend tout les infos de la carte et les envoie à la vue
-        $carte = Carte::find($idCompte);
+        $carte = Carte::where('idCompte', $idCompte)->first();
+
+
         $compte = Compte::find($idCompte);
         $logoSocial = Social::all();
         $social = Rediriger::where('idCarte', $carte->idCarte)
@@ -30,7 +31,6 @@ class Templates extends Controller
             ->get();
         $vue = Vue::where('idCarte', $carte->idCarte)->get();
         $template = Template::where('idTemplate', $idTemplate)->get();
-
 
         switch ($idTemplate) {
             case 1:
@@ -41,8 +41,11 @@ class Templates extends Controller
                 return view('Templates.peche', compact('carte', 'compte', 'social', 'vue', 'template', 'logoSocial'));
             default:
                 return abort(404, 'Template not found');
+         }
+
+        // return var_dump($carte, $idTemplate, $idCompte);
         }
-    }
+
 
     public function iframePomme()
     {
--- resources/views/Client/dashboardClient.blade.php
+++ resources/views/Client/dashboardClient.blade.php
@@ -13,6 +13,14 @@
 <div class="flex flex-col md:flex-row">
     @include('menu.menuClient')
     <div class="flex-1 md:ml-24 p-4">
+        @if($messageContent != "Aucun message disponible" || empty($messageContent))
+            <div class="bg-zinc-400 bg-opacity-45 border border-zinc-400 text-zin-700 px-4 py-3 rounded relative"
+                 role="alert">
+                <strong class="font-bold">Information :</strong>
+                <span class="block sm:inline">{{ $messageContent }}</span>
+            </div>
+        @endif
+
 
        <iframe src="{{ route('Templates.iframes.pomme') }}" width="100%" height="500px"></iframe>
         <iframe src="{{ route('Templates.iframes.peche') }}" width="100%" height="500px"></iframe>
