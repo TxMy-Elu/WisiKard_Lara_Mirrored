e4737fc2ca1325528d4509cb84638b70116472ba
TxMy <to.doguet@gmail.com>
2025-01-17 15:19:08 +0100

    Update dashboardClient.blade.php

--- resources/views/Client/dashboardClient.blade.php
+++ resources/views/Client/dashboardClient.blade.php
@@ -51,98 +51,127 @@
                 </a>
                     --}}
 
-            <div class="flex flex-wrap -mx-4">
-                <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-4">
-                    <div class="w-[600px] h-[350px] bg-white rounded-lg shadow-lg p-4 flex flex-col justify-between">
-                        <div class="flex justify-between">
-                            <!-- Title and other information -->
-                            <div class="flex flex-col">
-                                <div class="mb-4">
-                                    <p class="text-xl font-semibold">{{ $carte->nomEntreprise }}</p>
-                                </div>
-                                <div class="mb-4">
-                                    <p class="text-lg text-gray-600">{{ $carte->compte->email }}</p>
-                                </div>
-                                <!-- Phone number -->
-                                <div class="mb-4">
-                                    <p class="text-sm text-gray-600">{{ $carte->formattedTel }}</p>
-                                </div>
-                                <!-- Address -->
-                                <div>
-                                    <p class="text-sm text-gray-600">{{ $carte->ville }}</p>
-                                </div>
-                                @if($carte->compte->role == 'starter')
-                                    <div class="pt-4">
-                                        <div class="bg-blue-500 bg-opacity-65 border-solid border border-blue-500 rounded-full w-28 h-7 flex items-center justify-center">
-                                            <p class="text-slate-50 text-base">Starter</p>
-                                        </div>
-                                    </div>
-                                @elseif($carte->compte->role == 'advanced')
-                                    <div class="pt-4">
-                                        <div class="bg-violet-500 bg-opacity-65 border-solid border border-violet-500 rounded-full w-28 h-7 flex items-center justify-center">
-                                            <p class="text-slate-50 text-base">Advanced</p>
-                                        </div>
-                                    </div>
-                                @endif
+        <div class="flex flex-wrap -mx-4">
+            <div class="w-full sm:w-1/2 md:w-1/2 lg:w-1/3 xl:w-1/4 px-4 mb-4">
+                <div class="w-[600px] h-[350px] bg-white rounded-lg shadow-lg p-4 flex flex-col justify-between">
+                    <div class="flex justify-between">
+                        <!-- Title and other information -->
+                        <div class="flex flex-col">
+                            <div class="mb-4">
+                                <p class="text-xl font-semibold">{{ $carte->nomEntreprise }}</p>
                             </div>
-
-                            <!-- Logo -->
-                            <div class="justify-center mb-2">
-                                <div class="w-28">
-                                    <img src="{{ '/entreprises/'. $carte->compte->idCompte.'_'.$carte->nomEntreprise.'/logos/logo.jpg' }}"
-                                         alt="Logo"
-                                         class="w-28">
-                                </div>
+                            <div class="mb-4">
+                                <p class="text-lg text-gray-600">{{ $carte->compte->email }}</p>
                             </div>
+                            <!-- Phone number -->
+                            <div class="mb-4">
+                                <p class="text-sm text-gray-600">{{ $carte->formattedTel }}</p>
+                            </div>
+                            <!-- Address -->
+                            <div>
+                                <p class="text-sm text-gray-600">{{ $carte->ville }}</p>
+                            </div>
+                            @if($carte->compte->role == 'starter')
+                                <div class="pt-4">
+                                    <div class="bg-blue-500 bg-opacity-65 border-solid border border-blue-500 rounded-full w-28 h-7 flex items-center justify-center">
+                                        <p class="text-slate-50 text-base">Starter</p>
+                                    </div>
+                                </div>
+                            @elseif($carte->compte->role == 'advanced')
+                                <div class="pt-4">
+                                    <div class="bg-violet-500 bg-opacity-65 border-solid border border-violet-500 rounded-full w-28 h-7 flex items-center justify-center">
+                                        <p class="text-slate-50 text-base">Advanced</p>
+                                    </div>
+                                </div>
+                            @endif
                         </div>
 
-                        <!-- Buttons -->
-                        <div class="flex flex-row-reverse mt-auto pt-4">
-                            <a href="#" class="bg-indigo-500 text-white px-4 py-2 rounded-full mr-2">Modifier</a>
+                        <!-- Logo -->
+                        <div class="justify-center mb-2">
+                            <div class="w-28">
+                                <img src="{{ '/entreprises/'. $carte->compte->idCompte.'_'.$carte->nomEntreprise.'/logos/logo.jpg' }}"
+                                     alt="Logo"
+                                     class="w-28">
+                            </div>
                         </div>
+                    </div>
+
+                    <!-- Buttons -->
+                    <div class="flex flex-row-reverse mt-auto pt-4">
+                        <a href="#" class="bg-indigo-500 text-white px-4 py-2 rounded-full mr-2">Modifier</a>
                     </div>
                 </div>
+            </div>
 
-                <!-- QR Code -->
-                <div class="flex flex-col w-[400px] bg-white rounded-lg shadow-lg mx-auto p-4">
-                    <!-- QR Code Image -->
-                    <div class="mb-4 flex flex-col items-center">
-                        <img src="{{ '/entreprises/'. $carte->compte->idCompte.'_'.$carte->nomEntreprise.'/QR_Codes/QR_Code.svg' }}" alt="QR Code" class="w-full max-w-xs">
-                    </div>
-
-                    <!-- Form for Color Selection -->
-                    <form action="{{ route('dashboardClientColor') }}" method="POST" class="flex flex-col items-center w-full">
-                        @csrf
-                        <div class="flex">
-                        <div class="flex flex-col w-full mb-4">
-                            <label for="color1" class="w-full text-center mb-2">Pixel :</label>
-                            <input type="color" name="couleur1" id="color1" class="w-40 mx-auto" value="{{ $couleur1 }}">
-                        </div>
-                        <div class="flex flex-col w-full mb-4">
-                            <label for="color2" class="w-full text-center mb-2">Fond :</label>
-                            <input type="color" name="couleur2" id="color2" class="w-40 mx-auto" value="{{ $couleur2 }}">
-                        </div>
-                        </div>
-                        <button type="submit" class="w-full px-4 py-2 border border-gray-900 rounded-lg text-sm flex items-center justify-center hover:bg-gray-900 hover:text-white">
-                            Enregistrer
-                        </button>
-                    </form>
-
-                    <!-- Download Buttons -->
-                    <div class="flex flex-col items-center w-full mt-4 space-y-4">
-                        <a href="{{ route('downloadQrCodes') }}" class="w-full px-4 py-2 border border-gray-900 rounded-lg text-sm flex items-center justify-center hover:bg-gray-900 hover:text-white">
-                            Télécharger les QR Codes (noir et blanc)
-                        </a>
-                        <a href="{{ route('downloadQrCodesColor') }}" class="w-full px-4 py-2 border border-gray-900 rounded-lg text-sm flex items-center justify-center hover:bg-gray-900 hover:text-white">
-                            Télécharger les QR Codes (couleur)
-                        </a>
-                    </div>
+            <!-- QR Code -->
+            <div class="flex flex-col w-[400px] bg-white rounded-lg shadow-lg mx-auto p-4">
+                <!-- QR Code Image -->
+                <div class="mb-4 flex flex-col items-center">
+                    <img src="{{ '/entreprises/'. $carte->compte->idCompte.'_'.$carte->nomEntreprise.'/QR_Codes/QR_Code.svg' }}"
+                         alt="QR Code" class="w-full max-w-xs">
                 </div>
 
+                <!-- Form for Color Selection -->
+                <form action="{{ route('dashboardClientColor') }}" method="POST"
+                      class="flex flex-col items-center w-full">
+                    @csrf
+                    <div class="flex">
+                        <div class="flex flex-col w-full mb-4">
+                            <label for="color1" class="w-full text-center mb-0.5 font-bold">Pixel</label>
+                            <input type="color" name="couleur1" id="color1" class="w-40 mx-auto bg-white"
+                                   value="{{ $couleur1 }}">
+                        </div>
+                        <div class="flex flex-col w-full mb-4">
+                            <label for="color2" class="w-full text-center mb-0.5 font-bold">Fond</label>
+                            <input type="color" name="couleur2" id="color2" class="w-40 mx-auto bg-white"
+                                   value="{{ $couleur2 }}">
+                        </div>
+                    </div>
+                    <button type="submit" class="bg-indigo-500 text-white px-4 py-2 rounded-full w-[325px]">
+                        Enregistrer
+                    </button>
+                </form>
 
+                <div class="flex justify-center items-center text-center bg-white rounded-lg shadow-lg mx-auto my-2 w-[325px] p-2 mt-4 border border-gray-1200">
+                    <p class="font-bold text-xl">Télécharger</p>
+                </div>
+
+                <!-- Download Buttons -->
+                <div class="flex justify-center space-x-4 mt-4">
+                    <a href="{{ route('downloadQrCodesColor') }}"
+                       class="flex items-center justify-center px-4 py-2 bg-indigo-500 text-white rounded-lg text-sm hover:bg-indigo-600">
+                        Couleur
+                        <!-- Espace entre le texte et le SVG -->
+                        <span class="ml-2"></span>
+                        <!-- Download svg -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="feather feather-download">
+                            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                            <polyline points="7 10 12 15 17 10"></polyline>
+                            <line x1="12" y1="15" x2="12" y2="3"></line>
+                        </svg>
+                    </a>
+                    <a href="{{ route('downloadQrCodes') }}"
+                       class="flex items-center justify-center px-4 py-2 border border-gray-900 text-gray-900 rounded-lg text-sm hover:bg-gray-100">
+                        Noir / Blanc
+                        <!-- Espace entre le texte et le SVG -->
+                        <span class="ml-2"></span>
+                        <!-- Download svg -->
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none"
+                             stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"
+                             class="feather feather-download">
+                            <path d="M21 15v4a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-4"></path>
+                            <polyline points="7 10 12 15 17 10"></polyline>
+                            <line x1="12" y1="15" x2="12" y2="3"></line>
+                        </svg>
+                    </a>
+                </div>
 
             </div>
 
+
+        </div>
 
 
     </div>
