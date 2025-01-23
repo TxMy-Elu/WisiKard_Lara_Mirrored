e3e599ccf3e9b1f2c8c8651a43eaabd7948d3d01
TxMy <to.doguet@gmail.com>
2025-01-23 14:47:32 +0100

    destroy path

--- app/Http/Controllers/Entreprise.php
+++ app/Http/Controllers/Entreprise.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Carte;
 use App\Models\Compte;
+use Illuminate\Support\Facades\File;
 use Illuminate\Http\Request;
 
 class Entreprise extends Controller
@@ -18,6 +19,12 @@ class Entreprise extends Controller
         $compte = Compte::findOrFail($carte->idCompte);
         $compte->delete();
         $carte->delete();
+
+        //delete le dossier de l'entreprise
+        $path = public_path('entreprises/'.$compte->idCompte.'_'.$carte->nomEntreprise);
+        if (file_exists($path)) {
+            File::deleteDirectory($path);
+        }
 
         return redirect()->route('dashboardAdmin')->with('success', 'La carte a été supprimée avec succès.');
     }
