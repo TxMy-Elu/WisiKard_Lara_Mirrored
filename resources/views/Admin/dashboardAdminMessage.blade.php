facb4a1cb6c2046337d90b3228ebb907c8d5c89c
TxMy <to.doguet@gmail.com>
2025-01-24 16:50:37 +0100

    Dashboard Admin no-cdn

--- resources/views/Admin/dashboardAdminMessage.blade.php
+++ resources/views/Admin/dashboardAdminMessage.blade.php
@@ -4,24 +4,23 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard Admin</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"
             integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet"/>
 </head>
 <body class="bg-gray-100 w-full">
 
 <div class="flex flex-col md:flex-row">
     @include('menu.menuAdmin')
 
-    <div class="flex-1 md:ml-24 content">
+    <div class="flex-1 md:ml-24">
 
         <div class="min-h-screen p-4">
 
             <div class="flex flex-col md:flex-row justify-between items-center pb-4">
-                <!-- Link to the add information form -->
+                <!-- Lien vers le formulaire d'ajout d'information -->
                 <div class="flex items-center w-full md:w-auto">
                     <button type="button" onclick="openAddModal()"
                             class="w-full md:w-auto px-4 py-2 border border-gray-900 rounded-lg text-sm flex items-center justify-center hover:bg-gray-900 hover:text-white">
@@ -37,23 +36,24 @@
 
             <div class="flex flex-col space-y-4">
                 @foreach($messages as $message)
-                    <div class="flex flex-col md:flex-row items-center justify-between p-2 bg-white rounded shadow w-full">
+                    <div class="flex flex-col md:flex-row items-center justify-between p-4 bg-white rounded-lg shadow-md w-full">
                         <div class="flex-grow text-center md:text-left">
                             <p id="message-{{ $message->id }}">{{ $message->message }}</p>
                         </div>
                         <div class="flex items-center mt-2 md:mt-0">
                             <button type="button" onclick="toggleEdit({{ $message->id }})"
-                                    class="bg-indigo-500 text-white px-4 py-2 rounded mr-2">Modifier
+                                    class="bg-indigo-500 text-white px-4 py-2 rounded-lg mr-2">Modifier
                             </button>
-                            <form action="{{ route('toggleMessage', $message->id) }}" method="POST"
-                                  class="flex items-center">
+                            <form action="{{ route('toggleMessage', $message->id) }}" method="POST" class="flex items-center">
                                 @csrf
                                 @method('PATCH')
-                                <label class="toggle-switch">
+                                <!-- Basculer l'affichage de message -->
+                                <div class="relative inline-block w-10 mr-2 align-middle select-none">
                                     <input type="checkbox" name="afficher"
+                                           class="toggle-checkbox absolute block w-6 h-6 rounded-full bg-white border-4 border-gray-300 appearance-none cursor-pointer checked:right-0 checked:border-indigo-500"
                                            {{ $message->afficher ? 'checked' : '' }} onchange="this.form.submit()">
-                                    <span class="slider"></span>
-                                </label>
+                                    <label class="block overflow-hidden h-6 rounded-full bg-gray-300 cursor-pointer"></label>
+                                </div>
                             </form>
                         </div>
                     </div>
@@ -65,17 +65,17 @@
 
 <!-- Edit Modal Structure -->
 <div id="editModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden">
-    <div class="bg-white p-6 rounded shadow-lg w-11/12 md:w-1/2">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-11/12 md:w-1/2">
         <h2 class="text-xl mb-4">Modifier le message</h2>
         <form id="edit-form-modal" action="" method="POST">
             @csrf
             @method('PUT')
-            <textarea name="message" id="modal-message" class="border p-2 rounded w-full mb-4" rows="4"></textarea>
+            <textarea name="message" id="modal-message" class="border border-gray-300 p-2 rounded-lg w-full mb-4" rows="4"></textarea>
             <div class="flex justify-end">
                 <button type="button" onclick="closeModal('editModal')"
-                        class="bg-red-500 text-white px-4 py-2 rounded mr-2">Annuler
+                        class="bg-red-900 text-white px-4 py-2 rounded-lg mr-2">Annuler
                 </button>
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Enregistrer</button>
+                <button type="submit" class="bg-blue-900 text-white px-4 py-2 rounded-lg">Enregistrer</button>
             </div>
         </form>
     </div>
@@ -83,16 +83,16 @@
 
 <!-- Add Modal Structure -->
 <div id="addModal" class="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 hidden">
-    <div class="bg-white p-6 rounded shadow-lg w-11/12 md:w-1/2">
+    <div class="bg-white p-6 rounded-lg shadow-lg w-11/12 md:w-1/2">
         <h2 class="text-xl mb-4">Ajouter une Information</h2>
         <form id="add-form-modal" action="{{ route('ajoutMessage') }}" method="POST">
             @csrf
-            <textarea name="message" id="add-modal-message" class="border p-2 rounded w-full mb-4" rows="4"></textarea>
+            <textarea name="message" id="add-modal-message" class="border border-gray-300 p-2 rounded-lg w-full mb-4" rows="4"></textarea>
             <div class="flex justify-end">
                 <button type="button" onclick="closeModal('addModal')"
-                        class="bg-red-500 text-white px-4 py-2 rounded mr-2">Annuler
+                        class="bg-red-900 text-white px-4 py-2 rounded-lg mr-2">Annuler
                 </button>
-                <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded">Enregistrer</button>
+                <button type="submit" class="bg-blue-900 text-white px-4 py-2 rounded-lg">Enregistrer</button>
             </div>
         </form>
     </div>
--- resources/views/Admin/dashboardAdminStatistique.blade.php
+++ resources/views/Admin/dashboardAdminStatistique.blade.php
@@ -4,12 +4,11 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Dashboard Admin</title>
-    <script src="https://cdn.tailwindcss.com"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.1/chart.min.js"
             integrity="sha512-L0Shl7nXXzIlBSUUPpxrokqq4ojqgZFQczTYlGjzONGTDAcLremjwaWv5A+EDLnxhQzY5xUZPWLOLqYRkY0Cbw=="
             crossorigin="anonymous" referrerpolicy="no-referrer"></script>
     <script src="https://cdn.jsdelivr.net/npm/chart.js@4.4.7/dist/chart.umd.min.js"></script>
-    <link rel="stylesheet" href="{{ asset('css/styles.css') }}">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet" />
 </head>
 <body class="bg-gray-100 flex flex-col min-h-screen">
 
@@ -17,13 +16,14 @@
     @include('menu.menuAdmin')
 
     <div class="flex-1 md:ml-24 p-6">
-        <div class="w-full md:w-1/3 mx-auto p-6 bg-white rounded-lg border shadow-md mb-6 flex flex-col justify-between items-center">
+        <!-- Bloc Sélection Année -->
+        <div class="w-full md:w-1/3 mx-auto p-6 bg-white rounded-lg border border-gray-300 shadow-md mb-6 flex flex-col justify-between items-center">
             <div class="mb-4 w-full text-center">
                 <label for="yearSelect" class="block text-2xl font-bold text-gray-700">Sélectionnez l'année</label>
             </div>
             <form id="yearForm" action="{{ route('dashboardAdminStatistique') }}" method="get"
                   class="flex items-center w-full justify-center">
-                <select name="year" id="yearSelect" class="custom-select w-32 text-center">
+                <select name="year" id="yearSelect" class="w-32 text-center border border-gray-300 rounded-md px-4 py-2 text-gray-700 focus:outline-none focus:ring-2 focus:ring-indigo-500">
                     @foreach($years as $year)
                         <option value="{{ $year }}" @if($year == $selectedYear) selected @endif>{{ $year }}</option>
                     @endforeach
@@ -31,9 +31,10 @@
             </form>
         </div>
 
+        <!-- Statistiques et Graphique -->
         <div class="flex flex-wrap justify-center gap-6">
-            <!-- Compteur de nombre de vues total -->
-            <div class="w-full md:w-1/3 p-6 bg-white rounded-lg border shadow-md flex flex-col">
+            <!-- Compteur de vues total -->
+            <div class="w-full md:w-1/3 p-6 bg-white rounded-lg border border-gray-300 shadow-md flex flex-col">
                 <div class="mb-4">
                     <p class="text-center font-bold text-2xl">Nombre de vues</p>
                     <p class="text-center text-xl">Global</p>
@@ -43,8 +44,8 @@
                 </div>
             </div>
 
-            <!-- Compteur de nombre d'entreprises -->
-            <div class="w-full md:w-1/3 p-6 bg-white rounded-lg border shadow-md flex flex-col">
+            <!-- Compteur des entreprises -->
+            <div class="w-full md:w-1/3 p-6 bg-white rounded-lg border border-gray-300 shadow-md flex flex-col">
                 <div class="mb-4">
                     <p class="text-center font-bold text-2xl">Nombre d'Entreprises</p>
                     <p class="text-center text-xl">Global</p>
@@ -54,15 +55,15 @@
                 </div>
             </div>
 
-            <!-- Graph -->
-            <div class="w-full md:w-2/3 p-6 bg-white rounded-lg border shadow-md flex justify-center items-center">
+            <!-- Graphique Annuel -->
+            <div class="w-full md:w-2/3 p-6 bg-white rounded-lg border border-gray-300 shadow-md flex justify-center items-center">
                 <canvas id="yearChart" width="100" height="50"></canvas>
                 <script>
                     document.addEventListener('DOMContentLoaded', function () {
                         const yearlyData = @json($yearlyData);
                         const ctxYear = document.getElementById('yearChart').getContext('2d');
 
-                        // Yearly chart
+                        // Graphique annuel
                         let yearChart = new Chart(ctxYear, {
                             type: 'line',
                             data: yearlyData,
