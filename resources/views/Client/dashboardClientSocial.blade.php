363923c6892094a28519e0abbd29664da95d5e12
TxMy <to.doguet@gmail.com>
2025-01-27 10:30:44 +0100

    Dashboard Client twd-v4

--- resources/views/Client/dashboardClientSocial.blade.php
+++ resources/views/Client/dashboardClientSocial.blade.php
@@ -39,12 +39,13 @@
                                    value="{{ isset($activatedSocial[$reseau->idSocial]) ? $activatedSocial[$reseau->idSocial]['lien'] : '' }}"
                                    class="border border-gray-300 p-2 rounded mb-2 w-full"
                                    placeholder="Lien du réseau social">
-                            <div class="flex justify-between ">
+                            <div class="flex justify-between">
                                 <div class="flex items-center mb-2">
-                                    <label class="toggle-switch">
+                                    <label class="relative inline-flex items-center cursor-pointer">
                                         <input type="checkbox" id="{{ $reseau->idSocial }}"
-                                               name="activer" {{ isset($activatedSocial[$reseau->idSocial]) && $activatedSocial[$reseau->idSocial]['activer'] ? 'checked' : '' }}>
-                                        <span class="slider"></span>
+                                               name="activer" {{ isset($activatedSocial[$reseau->idSocial]) && $activatedSocial[$reseau->idSocial]['activer'] ? 'checked' : '' }}
+                                               class="sr-only peer">
+                                        <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                                     </label>
                                     <label for="{{ $reseau->idSocial }}" class="text-sm ml-2">Activer</label>
                                 </div>
@@ -60,13 +61,8 @@
                 <h2 class="text-xl text-white font-semibold">Ajouter un réseau social</h2>
             </div>
 
-
-            {{-- line / svg + --}}
             <div class="flex items-center justify-center mb-4">
-                <!-- Ligne à gauche -->
                 <div class="flex-grow h-px bg-gray-300"></div>
-
-                <!-- Rond avec un plus -->
                 <div id="toggleButton"
                      class="mx-4 flex items-center justify-center w-8 h-8 bg-indigo-500 text-white rounded-full cursor-pointer">
                     <svg class="w-4 h-4" fill="none" stroke="currentColor" viewBox="0 0 24 24"
@@ -75,17 +71,12 @@
                               d="M12 4v16m8-8H4"></path>
                     </svg>
                 </div>
-
-                <!-- Ligne à droite -->
                 <div class="flex-grow h-px bg-gray-300"></div>
             </div>
 
             <div class="flex flex-col items-center mb-4">
-
-                <!-- Formulaire caché -->
-
-                <form action="{{ route('dashboardClientCustomLink') }}" method="POST" class="flex flex-col hidden" {{-- Ajout de "hidden" ici --}}
-                id="hiddenForm">
+                <form action="{{ route('dashboardClientCustomLink') }}" method="POST" class="flex flex-col hidden"
+                      id="hiddenForm">
                     <div class="bg-white rounded-lg shadow-lg p-4 my-6">
                         @csrf
                         @method('POST')
@@ -104,39 +95,33 @@
                         </button>
                     </div>
                 </form>
-
             </div>
 
-
             <script>
-                // Sélectionner le bouton et le formulaire
                 const toggleButton = document.getElementById('toggleButton');
                 const hiddenForm = document.getElementById('hiddenForm');
 
-                // Écouter le clic sur le bouton
                 toggleButton.addEventListener('click', () => {
-                    // Basculer l'affichage du formulaire
                     hiddenForm.classList.toggle('hidden');
                 });
             </script>
-
 
             <div class="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
                 @foreach($custom as $link)
                     <div class="bg-white rounded-lg shadow-lg p-4 flex flex-col">
                         <div class="flex items-center mb-4">
-
-                            <p class="text-lg font-semibold ml-2"> {{$link->nom }}</p>
+                            <p class="text-lg font-semibold ml-2">{{ $link->nom }}</p>
                         </div>
                         <form action="{{ route('activeSocialLink') }}" method="POST" class="flex flex-col">
                             @csrf
-                            <input type="hidden" name="id_link" value="{{ $link->id_link}}"> {{-- Ajoutez ceci pour envoyer l'ID --}}
+                            <input type="hidden" name="id_link" value="{{ $link->id_link }}">
                             <input type="text" name="lien" value="{{ $link->lien }}" class="border border-gray-300 p-2 rounded mb-2 w-full" placeholder="Lien du réseau social">
                             <div class="flex justify-between">
                                 <div class="flex items-center mb-2">
-                                    <label class="toggle-switch">
-                                        <input type="checkbox" id="{{ $link->id_link }}" name="activer" {{ isset($activatedCustomLinks[$link->id_link]) && $activatedCustomLinks[$link->id_link]['activer'] ? 'checked' : '' }}>
-                                        <span class="slider"></span>
+                                    <label class="relative inline-flex items-center cursor-pointer">
+                                        <input type="checkbox" id="{{ $link->id_link }}" name="activer" {{ isset($activatedCustomLinks[$link->id_link]) && $activatedCustomLinks[$link->id_link]['activer'] ? 'checked' : '' }}
+                                        class="sr-only peer">
+                                        <div class="w-11 h-6 bg-gray-200 rounded-full peer peer-focus:ring-4 peer-focus:ring-blue-300 dark:peer-focus:ring-blue-800 dark:bg-gray-700 peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[2px] after:left-[2px] after:bg-white after:border-gray-300 after:border after:rounded-full after:h-5 after:w-5 after:transition-all dark:border-gray-600 peer-checked:bg-blue-600"></div>
                                     </label>
                                     <label for="activer-{{ $link->id }}" class="text-sm ml-2">Activer</label>
                                 </div>
@@ -146,8 +131,6 @@
                     </div>
                 @endforeach
             </div>
-
-
         </div>
     </div>
 </div>
