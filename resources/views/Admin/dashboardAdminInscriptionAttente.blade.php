34ac3402cba69a1c9cd9a68acc0130f996c56919
TxMy <to.doguet@gmail.com>
2025-02-11 12:04:08 +0100

    patch: ²

--- resources/views/Admin/dashboardAdminInscriptionAttente.blade.php
+++ resources/views/Admin/dashboardAdminInscriptionAttente.blade.php
@@ -60,7 +60,6 @@
     @include('menu.menuAdmin')
     <div class="flex-1 md:ml-24 content"> <!-- Adjusted margin-left to match the new menu width -->
         @if($messageContent != "Aucun message disponible" || empty($messageContent))
-            ²
             <div class="bg-zinc-400/45 border border-zinc-400 text-zin-700 px-4 py-3 rounded relative"
                  role="alert">
                 <strong class="font-bold">Information :</strong>
