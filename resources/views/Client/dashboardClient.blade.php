6e9ffbe38b5c95877e9cd4c71b6d37fe68be77e0
TxMy <to.doguet@gmail.com>
2025-02-14 16:25:15 +0100

    patch: logo

--- resources/views/Client/dashboardClient.blade.php
+++ resources/views/Client/dashboardClient.blade.php
@@ -160,6 +160,9 @@
                             <div class="logo-container">
                                 @if($logoPath)
                                     <img src="{{ $logoPath }}" alt="Logo" class="w-full h-auto rounded-lg">
+                                    @php
+                                        echo $logoPath;
+                                    @endphp
                                 @else
                                     <svg xmlns="http://www.w3.org/2000/svg" class="w-12 h-12 text-gray-400"
                                          viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2"
@@ -168,6 +171,9 @@
                                         <circle cx="8.5" cy="8.5" r="1.5"></circle>
                                         <polyline points="21 15 16 10 5 21"></polyline>
                                     </svg>
+                                    @php
+                                        echo $logoPath;
+                                    @endphp
                                 @endif
                             </div>
 
